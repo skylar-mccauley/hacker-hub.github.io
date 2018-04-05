77402be75bd2eb8edf87f19232e9352020e58888
HackerHub <38106936+Hacker-Hub@users.noreply.github.com>
2018-04-05 10:33:31 -0400

    Add files via upload

---
+++ redirect/index.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start(); 
+
+echo "<!DOCTYPE html> 
+<html>
+
+    <head>
+        <title>HackerHub - Redirect</title>
+        <script>
+        
+        </script>
+    </head> 
+
+    <body>
+
+    </body>
+<strong>Warning: </strong> You are about to be redirected to 
+
+
+</html>";
+
+?>
