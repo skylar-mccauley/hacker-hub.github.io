b7e08e067fe5a5cc49b85bcd401dea03203c35fa
The-Hacker894 <skylarmccauley864@gmail.com>
2018-05-11 21:05:11 -0400

    fixed iedetect

--- js/iedetect.js
+++ js/iedetect.js
@@ -1,7 +1,7 @@
 //var badbrowser = ['Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko','Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv 11.0) like Gecko','Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko', 'MSIE', 'MSIE 8.0']
 
 function redirect() {
-    window.location.replace='https://hacker-hub.github.io'
+    window.location.replace='https://hacker-hub.github.io/ie'
 }
 var ua = window.navigator.userAgent;
     var msie = ua.indexOf("MSIE ");
