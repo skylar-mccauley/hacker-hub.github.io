3a47ffeae97a229f2a2bbec527775fc7e0ca70fa
TheHacker894 <skylarmccauley864@gmail.com>
2018-06-05 16:38:15 -0400

    Delete ads.js

--- ads.js
+++
@@ -1,4 +0,0 @@
-var e=document.createElement('div');
-e.id='STXdswJoWKuDyvV6iaJLIPD4Ft0nUeEuzcK4Dlj7DPzHJDefd8E0VQ90530QS00gwiPAPceUdqlOlrNQS00gwiPAPceUdqlOlrNQ';
-e.style.display='none';
-document.body.appendChild(e);
