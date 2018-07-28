fe1b29f7fa694bb76a2313c95fffea495756b410
The-Hacker894 <skylarmccauley864@gmail.com>
2018-07-28 10:37:42 -0400

    Small tweaks & new buttons

--- js/deviceDetect.js
+++ js/deviceDetect.js
@@ -1,20 +1,29 @@
-var isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-    };
+var screenHeight = parseFloat(window.screen.availHeight)
+var screenWidth = parseFloat(window.screen.availWidth)
+var smallestWidth = parseFloat(696)
+var otherSmallestWidth = parseFloat(600)
+
+var initLog = {
+    "screenHeight": screenHeight,
+    "screenWidth": screenWidth,
+    "smallestWidth": smallestWidth,
+    "otherSmallestWidth": otherSmallestWidth
+}
+
+function toggleDisplay(id)
+            {
+                var el = document.getElementById(id);
+                if(el.style.display == "none") el.style.display = "block";
+                else el.style.display = "none";
+            }
+console.log(initLog)
+if(screenHeight < screenWidth) {
+    console.log('Height '+ initLog.screenHeight + ' is less than Width ' + initLog.screenWidth)
+// If device is in landscape mode
+
+if((screenWidth < smallestWidth) && (screenWidth > otherSmallestWidth)) {
+    console.log('Width ' + initLog.screenWidth + ' is greater than ' + initLog.smallestWidth + ' and Width ' + initLog.screenWidth + ' is greater than the Override Width ' + initLog.otherSmallestWidth)
+    $('#displayNotSupported').show()
+}
+
+}
