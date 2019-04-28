7f7578cbb760ac8c539f683b2a0fd14deba1518f
The-Hacker894 <skylarmccauley894@gmail.com>
2019-04-28 10:51:17 -0400

    Added ads

--- hacked-repo/config.py
+++
@@ -1,2 +0,0 @@
-TemplatesPath = 'depictions/templates/' # Path to the templates folder.
-SaveDepictionPath = 'depictions/user/templates/' # Path where the depition file will be saved to once it's created.
